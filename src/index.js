a1be7e6262ac1f31a7ef7e2b95a96efd84f81367
gabriel reis <gabrielreisnf@gmail.com>
2018-01-19 12:43:25 -0200

    adding fragmentFetch script and fragmentMatcher into cache

--- src/index.js
+++ src/index.js
@@ -4,18 +4,27 @@ import './css/index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-import {ApolloProvider} from 'react-apollo';
-import {ApolloClient} from 'apollo-client';
-import {createHttpLink} from 'apollo-link-http';
-import {InMemoryCache} from 'apollo-cache-inmemory';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+import { IntrospectionFragmentMatcher } from 'apollo-cache-inmemory';
+import introspectionQueryResultData from './fragmentTypes.json';
+
+const fragmentMatcher = new IntrospectionFragmentMatcher({
+  introspectionQueryResultData
+});
+
+const cache = new InMemoryCache({ fragmentMatcher });
 
 const link = createHttpLink({
   uri: 'https://cors-anywhere.herokuapp.com/https://app.pipefy.com/public_api',
 });
 
 const client = new ApolloClient({
+  cache,
   link: link,
-  cache: new InMemoryCache(),
 });
 
 ReactDOM.render(
